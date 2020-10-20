ca6b04a3a7bb209df1eeaa77e61f2cec18ddbaa3
jaechang <jaechang0721k@gmail.com>
2020-10-20 14:59:42 +0900

    PF

---
+++ Assets/FenceControl.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FenceControl : MonoBehaviour
+{
+
+    public List<GameObject> BossSceneMobList;
+
+    void Start()
+    {
+        
+    }
+
+    
+    void Update()
+    {
+        
+    }
+
+
+    private void CheckMob()
+    {
+        for (int i = 0; i < BossSceneMobList.Count; i++)
+        {
+            if (BossSceneMobList[i].activeSelf == true)
+            {
+                return;
+            }
+        }
+
+    }
+
+
+}
